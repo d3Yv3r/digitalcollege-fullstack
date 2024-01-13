475741c900de91e8dcff006d4c8725254cb8e9ef
d3Yv3r <deyverson@astrotech.solutions>
2024-01-13 16:54:59 -0300

    Adicionando aula 15

---
+++ Aula-15-Boolean/array.js
@@ -0,0 +1,17 @@
+let produtos = ["iPhone", "Nike", "Xilito"];
+
+// console.log(produtos.length);
+
+/*
+  for (let indice = 0; indice < produtos.length; indice++) {
+    console.log(produtos[indice]);
+  }
+*/
+
+/*
+  for (let indice = produtos.length - 1; indice >= 0; indice--) {
+    console.log(produtos[indice]);
+  }
+*/
+
+console.log(produtos.reverse());
---
+++ Aula-15-Boolean/boolean.js
@@ -0,0 +1,21 @@
+let idade = 20;
+let idade2 = "20";
+let semValor;
+
+/*
+if (semValor) {
+  console.log("Entrou no if");
+} else {
+  console.log("Não entrou no if");
+}
+*/
+
+/*
+let valorTernario =
+  idade == 10 ? "Idade é igual a 20" : "Idade não é igual a 20";
+console.log(valorTernario);
+*/
+
+idade += 5;
+idade -= 5;
+console.log(idade);
